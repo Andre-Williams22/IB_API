842b0199e1e013e57811b9b6691d06af31fac22c
Andre Williams <awjrs22@gmail.com>
2021-01-10 13:52:42 -0500

    asynchronous implementation

--- ib_basic_app.py
+++ ib_basic_app.py
@@ -9,6 +9,8 @@ from ibapi.client import EClient
 # helps us tanslate data in a way python can understand
 from ibapi.wrapper import EWrapper 
 from ibapi.contract import Contract 
+import threading
+import time 
 
 class TradingApp(EWrapper, EClient):
     
@@ -22,11 +24,23 @@ class TradingApp(EWrapper, EClient):
         
     def contractDetails(self, reqId, contractDetails):
         print("redID: {}, contract:{}".format(reqId, contractDetails))
+    
         
+    
+def websocket_connection():
+    # creates our connection
+    app.run()
         
-        
+
+
 app = TradingApp()
 app.connect("127.0.0.1", 7497, clientId=1)
+
+    
+con_thread = threading.Thread(target=websocket_connection, daemon=True)
+con_thread.start()
+time.sleep(1)
+
 
 contract = Contract()
 contract.symbol = "AAPL"
@@ -36,9 +50,10 @@ contract.exchange = "SMART"
 
 
 app.reqContractDetails(100, contract)
+# closes after 5 seconds
+time.sleep(5)
 
 
-app.run()
 
          
     
